1ae56aa75f0c8cf213e9110a6060aa1427870ca7
Pratik Sharma <contact@drafteq.com>
2023-08-31 11:52:28 +0530

    added temp

---
+++ src/temp/next-config-plugins.js
@@ -0,0 +1,4 @@
+exports.graphqlPlugin = require('../lib/next-config/plugins/graphql');
+exports.robotsPlugin = require('../lib/next-config/plugins/robots');
+exports.sassPlugin = require('../lib/next-config/plugins/sass');
+exports.sitemapPlugin = require('../lib/next-config/plugins/sitemap');
