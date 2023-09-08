cc2ada826da356d07c2b85c8fc0142966f5fe541
Pratik Sharma <contact@drafteq.com>
2023-09-08 14:40:46 +0530

    pixels commit

--- src/components/FlightAvailability/FlightAvailability.tsx
+++ src/components/FlightAvailability/FlightAvailability.tsx
@@ -1944,8 +1944,6 @@ const FlightAvailability = () => {
       ) : (
         load.name === 'exp' && <SavingExpLoader open={load?.show} />
       )}
-      {/* <!-- START: Pixel '79124014' --> */}
-      <img src='https://ipac.ctnsnet.com/int/integration?pixel=79124014&nid=2142538&cont=i' width='1' height='1' border='0' alt=''/>
       
       <Image 
         src='https://ipac.ctnsnet.com/int/integration?pixel=79124014&nid=2142538&cont=i'
