8738eb301110b5340d71117a98ed2c5e6df0834b
Pratik Sharma <contact@drafteq.com>
2023-09-08 13:44:19 +0530

    pixels commit

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -42,6 +42,13 @@ const Layout = ({ layoutData }: LayoutProps): JSX.Element => {
         <meta name="description" content={fields?.MetaDescription?.value?.toString() || 'Description'} />
         <meta name="keywords" content={fields?.MetaKeywords?.value?.toString() || 'Keywords'} />
         <link rel="icon" href={`${publicUrl}/favicon.ico`} />
+        <script src="https://consentag.eu/public/3.1.1/consenTag.js"></script>
+        <script type="text/javascript">
+            consenTag.init({
+                containerId: "79117570",
+                silentMode: true
+            }, true);
+        </script>
       </Head>
 
       {/* root placeholder for the app, which we add components to using route data */}
@@ -57,14 +64,6 @@ const Layout = ({ layoutData }: LayoutProps): JSX.Element => {
         </section>
       </div>
       <Analytics />
-      <script src="https://consentag.eu/public/3.1.1/consenTag.js"></script>
-        <script type="text/javascript">
-            consenTag.init({
-                containerId: "79117570",
-                silentMode: true
-            }, true);
-      </script>
-      {/* <!-- END: 79124012 pixels, are aggregated in consenTag container --> */}
     </>
   );
 };
