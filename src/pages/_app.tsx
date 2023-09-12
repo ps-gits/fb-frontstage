31c1342842c4b83d339f315d0e9b7d7bc2fef5e7
Pratik Sharma <contact@drafteq.com>
2023-09-13 02:49:32 +0530

    adroll

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -88,7 +88,7 @@ function App({ Component, pageProps }: AppProps<SitecorePageProps>): JSX.Element
           w.__adroll_loaded = true;
           w.adroll = w.adroll || [];
           w.adroll.f = [ 'setProperties', 'identify', 'track' ];
-          var roundtripUrl = https://s.adroll.com/j/ + adroll_adv_id
+          var roundtripUrl = "https://s.adroll.com/j/" + adroll_adv_id
                   + "/roundtrip.js";
           for (a = 0; a < w.adroll.f.length; a++) {
               w.adroll[w.adroll.f[a]] = w.adroll[w.adroll.f[a]] || (function(n) {
@@ -124,40 +124,6 @@ function App({ Component, pageProps }: AppProps<SitecorePageProps>): JSX.Element
             // {{`consenTag.init({containerId: "79117570",silentMode: true}, true)`}}}
           setIsLoad(true)}
       />
-      {/* <Script
-      id='addroll'
-      dangerouslySetInnerHTML={{
-        __html: `
-        console.log("insude tis")
-        adroll_adv_id = "HHDIC2MYFZC7NJPXQ25UIH";
-    adroll_pix_id = "IMDARVLXGJEUZF5RSPKPO6";
-    adroll_version = "2.0";
-
-    (function(w, d, e, o, a) {
-        w.__adroll_loaded = true;
-        w.adroll = w.adroll || [];
-        w.adroll.f = [ 'setProperties', 'identify', 'track' ];
-        var roundtripUrl = https://s.adroll.com/j/ + adroll_adv_id
-                + "/roundtrip.js";
-        for (a = 0; a < w.adroll.f.length; a++) {
-            w.adroll[w.adroll.f[a]] = w.adroll[w.adroll.f[a]] || (function(n) {
-                return function() {
-                    w.adroll.push([ n, arguments ])
-                }
-            })(w.adroll.f[a])
-        }
-
-        e = d.createElement('script');
-        o = d.getElementsByTagName('script')[0];
-        e.async = 1;
-        e.src = roundtripUrl;
-        o.parentNode.insertBefore(e, o);
-    })(window, document);
-    adroll.track("pageView");
-        `,
-      }}
-    
-      /> */}
         
       {mount && (
         <Provider store={store}>
