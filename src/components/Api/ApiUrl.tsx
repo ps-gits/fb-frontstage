abf9be37e9011bfacb69165cc53631cfa7a63c46
Pratik Sharma <contact@drafteq.com>
2023-09-15 16:15:54 +0530

    api

--- src/components/Api/ApiUrl.tsx
+++ src/components/Api/ApiUrl.tsx
@@ -1,7 +1,11 @@
-export const url = 'https://api.scxmcloudbeond.com/'
+// export const url = 'https://api.scxmcloudbeond.com/'
 // export const url = 'https://flight.manageprojects.in/';
-// export const url = 'https://apis.flybeond.com/';
+export const url = 'https://apis.flybeond.com/';
 
 export const graphQLUrl = 'https://edge.sitecorecloud.io/api/graphql/v1';
 export const graphQLToken =
   'ampjVGM4ODNnRTZEa0RjSFNrckx0ZXpQenJEZHlHYUFqbmJwQUh0RlNWVT18YXJhYmVzcXVlZmwwZjcwLWRlbW9wcm9qZWN0LWRlbW9lbnYtNzliYw==';
+
+export const subscribeKey = '0a46a248-b4d6-48ec-bd15-9fc0d1278191';
+export const subscribeUrl =
+  'https://api.sitecoresend.io/v3/subscribers/838db35f-bedd-4acd-bff7-a811ada0c88f/subscribe.json?apikey=';
