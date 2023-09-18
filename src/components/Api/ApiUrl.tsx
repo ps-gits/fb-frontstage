188177e5ffa2eb39b908b3a0b0180722cf251c72
Pratik Sharma <contact@drafteq.com>
2023-09-18 15:45:31 +0530

    update age

--- src/components/Api/ApiUrl.tsx
+++ src/components/Api/ApiUrl.tsx
@@ -6,6 +6,6 @@ export const graphQLUrl = 'https://edge.sitecorecloud.io/api/graphql/v1';
 export const graphQLToken =
   'ampjVGM4ODNnRTZEa0RjSFNrckx0ZXpQenJEZHlHYUFqbmJwQUh0RlNWVT18YXJhYmVzcXVlZmwwZjcwLWRlbW9wcm9qZWN0LWRlbW9lbnYtNzliYw==';
 
-export const subscribeKey = '0a46a248-b4d6-48ec-bd15-9fc0d1278191';
+export const subscribeKey = '8a29f63e-5641-4ae8-897d-410c6216effe';
 export const subscribeUrl =
   'https://api.sitecoresend.io/v3/subscribers/838db35f-bedd-4acd-bff7-a811ada0c88f/subscribe.json?apikey=';
